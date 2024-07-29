0a813b4a99ba4952f4e5607fdcc6f646a7265ab2
ervinpopescu <ervin.popescu10@gmail.com>
2024-07-30 01:09:07 +0300

    [chore] add `simple_logger`

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ pub(crate) mod utils;
 use utils::{args::Args, client::ShellClient};
 
 fn main() -> anyhow::Result<()> {
+    simple_logger::SimpleLogger::new()
+        .with_level(log::LevelFilter::Info)
+        .env()
+        .init()?;
     let args = Args::parse();
     // println!("{:#?}", args);
     ShellClient::call(args)
