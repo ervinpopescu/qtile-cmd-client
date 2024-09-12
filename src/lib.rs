df3277eb733a329938a029833209e50aaf80079a
Ervin Popescu <84532402+ervinpopescu@users.noreply.github.com>
2024-09-13 02:54:40 +0300

    [test(x11)] what happens in CI, stays in CI (#16)
    After a long journey, we're finally here.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,8 @@
 //! I've written this qtile client in order to learn some more Rust (get used to [`serde`], [`clap`] and
 //! other crates) and to get faster responses than when I used `qtile cmd-obj` and other such
 //! commands (which were most likely slower because of the underlying Python bonanza).
-#![warn(
-    dead_code,
-    unused_variables,
-    unreachable_code,
-    unused_imports,
-    missing_docs
-)]
+#![warn(dead_code, unused_variables, unreachable_code, unused_imports)]
+
 #[cfg(test)]
 pub mod tests;
 /// Utilities for interacting with Qtile
