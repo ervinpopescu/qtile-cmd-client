1684f0cafd4fb871cfa6e584a3e0a970b8077d69
ervinpopescu <ervin.popescu10@gmail.com>
2026-03-01 15:54:15 +0200

    refactor: clean up dead code and enhance documentation

--- src/utils/graph.rs
+++ src/utils/graph.rs
@@ -1,24 +1,8 @@
-// use std::{collections::HashMap, sync::Mutex};
-// use once_cell::sync::Lazy;
-
 use anyhow::Error;
 
 pub(crate) static OBJECTS: &[&str] = &[
     "core", "screen", "bar", "widget", "group", "layout", "window", "root",
 ];
-
-// pub(crate) static OBJ_HASHMAP: Lazy<Mutex<HashMap<&str, ObjectType>>> = Lazy::new(|| {
-//     let mut hashmap = HashMap::new();
-//     hashmap.insert("root", ObjectType::Root);
-//     hashmap.insert("core", ObjectType::Core);
-//     hashmap.insert("screen", ObjectType::Screen(None));
-//     hashmap.insert("group", ObjectType::Group(None));
-//     hashmap.insert("layout", ObjectType::Layout(None));
-//     hashmap.insert("window", ObjectType::Window(None));
-//     hashmap.insert("bar", ObjectType::Bar(None));
-//     hashmap.insert("widget", ObjectType::Widget(None));
-//     hashmap.into()
-// });
 
 /// Basically a Union
 #[allow(dead_code)]
@@ -44,6 +28,7 @@ impl Clone for Selector {
 }
 
 /// Qtile has a couple of object types which we save here for easy disambiguation.
+#[allow(dead_code)]
 #[derive(Clone, Debug)]
 pub enum ObjectType {
     /// Screens are the display area that holds bars and an active group. Screen commands include changing the current group and changing the wallpaper.
@@ -103,7 +88,7 @@ impl ObjectType {
             "bar" => Ok(Self::Bar(Some(s))),
             "group" => Ok(Self::Group(Some(s))),
             "widget" => Ok(Self::Widget(Some(s))),
-            _ => Err(Error::msg("Failed to parse ".to_string() + string)),
+            _ => Err(Error::msg(format!("Failed to parse {string}"))),
         }
     }
     /// For:
@@ -116,13 +101,14 @@ impl ObjectType {
             "screen" => Ok(Self::Screen(Some(n))),
             "layout" => Ok(Self::Layout(Some(n))),
             "window" => Ok(Self::Window(Some(n))),
-            _ => Err(Error::msg("Failed to parse ".to_string() + string)),
+            _ => Err(Error::msg(format!("Failed to parse {string}"))),
         }
     }
     /// For:
     /// - [`Core`](ObjectType::Core)
     /// - [`Root`](ObjectType::Root)
     /// - [`None`]-initialized variants
+    #[allow(dead_code)]
     pub fn with_none(string: &str) -> anyhow::Result<ObjectType> {
         match string {
             "screen" => Ok(Self::Screen(None)),
@@ -133,101 +119,7 @@ impl ObjectType {
             "widget" => Ok(Self::Widget(None)),
             "core" => Ok(Self::Core),
             "root" => Ok(Self::Root),
-            _ => Err(Error::msg("Failed to parse ".to_string() + string)),
-        }
-    }
-}
-
-type SelectorType = (ObjectType, Selector);
-
-/// A combination of the concepts from qtile of `CommandGraphNode`, `CommandGraphRoot` and
-/// CommandGraphCall. See
-/// [qtile docs](https://docs.qtile.org/en/latest/manual/commands/advanced.html#the-command-graph) for more
-/// details
-#[allow(dead_code)]
-#[derive(Clone, Debug)]
-pub struct CommandGraphNode {
-    selector: Option<Selector>,
-    selectors: Vec<SelectorType>,
-    parent: Option<Box<Self>>,
-    children: Option<Vec<String>>,
-    typ: ObjectType,
-}
-
-impl Default for CommandGraphNode {
-    fn default() -> Self {
-        Self {
-            selector: None,
-            selectors: vec![],
-            parent: None,
-            children: [
-                "bar", "group", "layout", "screen", "widget", "window", "core",
-            ]
-            .iter()
-            .map(|&s| s.to_string())
-            .collect::<Vec<String>>()
-            .into(),
-            typ: ObjectType::Root {},
-        }
-    }
-}
-
-#[allow(dead_code)]
-impl CommandGraphNode {
-    /// Create a new CommandGraphNode using a `selector`, a `parent` and a `type`
-    pub fn new(selector: Selector, parent: Option<Box<Self>>, typ: ObjectType) -> Self {
-        fn new_with_children(
-            selector: Selector,
-            parent: Option<Box<CommandGraphNode>>,
-            typ: ObjectType,
-            children: &[&str],
-        ) -> CommandGraphNode {
-            let mut selectors = parent.clone().unwrap_or_default().clone().selectors;
-            selectors.extend(vec![(typ.clone(), selector.clone())]);
-            let children = children
-                .iter()
-                .map(|&s| s.to_string())
-                .collect::<Vec<String>>()
-                .into();
-            CommandGraphNode {
-                selector: Some(selector),
-                selectors,
-                parent,
-                children,
-                typ,
-            }
-        }
-        match typ {
-            ObjectType::Root => Self::default(),
-            ObjectType::Bar(_) => {
-                let children = ["screen", "widget"];
-                new_with_children(selector, parent, typ, &children)
-            }
-            ObjectType::Group(_) => {
-                let children = ["layout", "window", "screen"];
-                new_with_children(selector, parent, typ, &children)
-            }
-            ObjectType::Layout(_) => {
-                let children = ["screen", "group", "window"];
-                new_with_children(selector, parent, typ, &children)
-            }
-            ObjectType::Screen(_) => {
-                let children = ["layout", "window", "bar", "widget", "group"];
-                new_with_children(selector, parent, typ, &children)
-            }
-            ObjectType::Widget(_) => {
-                let children = ["bar", "screen"];
-                new_with_children(selector, parent, typ, &children)
-            }
-            ObjectType::Window(_) => {
-                let children = ["layout", "group", "screen"];
-                new_with_children(selector, parent, typ, &children)
-            }
-
-            ObjectType::Core => {
-                let children = [];
-                new_with_children(selector, parent, typ, &children)
-            }
+            _ => Err(Error::msg(format!("Failed to parse {string}"))),
         }
     }
 }
